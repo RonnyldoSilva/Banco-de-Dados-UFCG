1bcef5274a6721e6a8b06b799a59bbe1125bd091
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2018-10-14 10:33:14 -0300

    Q6.

--- Roteiros/roteiro5.sql
+++ Roteiros/roteiro5.sql
@@ -13,3 +13,17 @@ SELECT e.superssn AS ssn_supervisor, count(*) AS qtd_supervisionados
 FROM employee AS e 
 GROUP BY e.superssn 
 ORDER BY count(*);
+
+--Q4
+
+--Q5
+
+--Q6
+SELECT MIN(qtd) AS qtd 
+FROM (
+  SELECT count(*) AS qtd 
+  FROM works_on AS w 
+  GROUP BY w.pno
+) 
+AS foo;
+
