9e73aa3292cbffd78c72eb7b8304874973abd6ab
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2019-11-10 22:58:33 -0300

    Update mini teste 3.sql

--- mini teste 3/mini teste 3.sql
+++ mini teste 3/mini teste 3.sql
@@ -2,7 +2,8 @@ create table Estudante(matricula int unique not null, nome varchar(30), data_nas
 create table Disciplina(codigo int unique not null, carga_horaria int);
 create table EstudanteDisciplina(matricula int, disciplina int, periodo_letivo int);
 alter table estudantedisciplina add foreign key (matricula) references estudante(matricula);
-alter table estudantedisciplina add foreign key (disciplina) references disciplina(codigo); 
+alter table estudantedisciplina add foreign key (disciplina) references disciplina(codigo);
+
 
 
 
